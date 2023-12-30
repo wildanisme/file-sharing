6aaf0a0c838ed9b804d51058e7a0b58e9e718d91
wildanisme <wildantafufiq.abdulaziz@gmail.com>
2023-12-30 17:30:32 +0700

    add upload file method

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class FileController extends Controller
 {
     public function upload(Request $request)
     {
+
+        $validator = Validator::make($request->all(),[
+            'filename'  => 'string',
+            'file'      => 'file'
+        ]);
+
         $path = 'public/';
         $file = $request->file('file');
         $extension = $file->getClientOriginalExtension();
